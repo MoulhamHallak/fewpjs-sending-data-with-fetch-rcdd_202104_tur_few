0ad7fc577e0307eb221b8336b56a5e326fdbf4da
Moulham Hallak <mou990@gmail.com>
2021-06-20 23:09:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 // Add your code here
 function submitData(name, email) {
-    let formData = {
-        name: name,
-        email: email
-    };
-
-    let configObj = {
+    return fetch('http://localhost:3000/users', {
         method: "POST",
         headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json"
+            "Content-Type": "application/json",
+            "Accept": "application/json"
         },
-        body: JSON.stringify(formData)
-    };
-
-    return fetch("http://localhost:3000/users", configObj)
-        .then(function(response) {
-          return response.json();
+        body: JSON.stringify( {
+            name,
+            email
         })
-        .then(function(object) {
-            let h2 = document.createElement('h2');
-            h2.innerHTML = object.id;
-            document.body.appendChild(h2);
-            console.log(object);
-        })
-        .catch(function(error) {
-            let h3 = document.createElement('h3');
-            h3.innerHTML = error.message;
-            document.body.appendChild(h3);
-            console.log(error.message);
-        });
+    })
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(object){
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function(error){
+        document.body.innerHTML = error.message
+    })
 }
